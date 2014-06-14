50ae544189945415d1ee97cb8ddf0a39a9501193
Nouran Mahmoud <nouran.mhmoud@gmail.com>
2014-06-14 20:18:26 +0000

    Drawing on the map steps
    Generating directions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,16 @@ class PagesController < ApplicationController
   def home
   end
   
+  def geocoding
+    respond_to do |format|               
+      format.js
+    end
+  end
+  
+  def reverse_geocoding
+    respond_to do |format|               
+      format.js
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ GoogleMapsTut::Application.routes.draw do
   root "pages#home"
   
   get "/home", to: "pages#home", as: "home"
+  get "/geocoding", to: "pages#geocoding", as: "geocoding"
+  get "/reverse_geocoding", to: "pages#reverse_geocoding", as: "reverse_geocoding"
 end
