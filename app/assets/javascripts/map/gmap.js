60475b784f7284731901f343f411eff38f967bf2
Nouran Mahmoud <nouran.mhmoud@gmail.com>
2014-07-03 20:02:22 +0000

    Drawing polyline with animation

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -7,6 +7,7 @@ var poly;
 function initialize() {
         var markerCoords1 = new google.maps.LatLng(30.055487, 31.279766);
         var markerCoords2 = new google.maps.LatLng(30.543523, 31.656232);
+       
   var mapOptions = {
           center: new google.maps.LatLng(30.055487, 31.279766),
           zoom: 8,
@@ -18,71 +19,87 @@ function initialize() {
         };
         // initializing map
         map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-        // simple marker
-        var marker = createMarker(markerCoords1, map, "Hi");
+       
+        // drawing dynamic polyline
+          var polyOptions = {
+            strokeColor: '#000000',
+            strokeOpacity: 1.0,
+            strokeWeight: 3
+          };
+        poly = new google.maps.Polyline(polyOptions);
+        poly.setMap(map);
+        google.maps.event.addListener(map, 'click', addLatLng);
         
-        // custom marker
-        var customMarker = createCustomMarker(markerCoords2, map, "Hi");
+        // drawing custom dynamic polyline ( with dashes/ symbols)
+        // Define the symbol, using one of the predefined paths ('CIRCLE')
+        // supplied by the Google Maps JavaScript API.
+        var lineSymbol = {
+          path: 'M 0,-1 0,1',
+          scale: 4,
+          strokeOpacity: 1,
+          strokeColor: '#393'
+        };
+        // drawing static polyline
+        var lineCoordinates = [
+          new google.maps.LatLng(30.055487, 31.279766),
+          new google.maps.LatLng(30.223356, 31.324345),
+          new google.maps.LatLng(30.345656, 31.567677),
+          new google.maps.LatLng(30.565678, 31.676887)
+        ];
+        createPolyline(map,lineCoordinates,lineSymbol);
+        animateCircle();
 
-        // add infowindow when clicking on the simple marker marker
-        var info = createInfoWindow("Congratulation!");
-        google.maps.event.addListener(marker, 'click', function() {
-          info.open(map,marker);
-        });
+         // drawing dynamic polyline
+          var polyOptions = {
+            strokeColor: '#000000',
+            strokeOpacity: 1.0,
+            strokeWeight: 3,
+            draggable:true,
+            editable: true
+          };
+        polyg = new google.maps.Polyline(polyOptions);
+        polyg.setMap(map);
+        google.maps.event.addListener(map, 'click', addLatLng);
 
-        // drawing points
-        google.maps.event.addListener(map, 'click', function(e){
-          createMarker(getCurrentPosition(e), map, "Hello World!");
-        });
 }
-function createInfoWindow(text){
-  var infowindow = new google.maps.InfoWindow({
-    content: text
+
+
+function animateCircle() {
+    var count = 0;
+    window.setInterval(function() {
+      count = (count + 1) % 200;
+
+      var icons = linePath.get('icons');
+      icons[0].offset = (count / 2) + '%';
+      linePath.set('icons', icons);
+  }, 20);
+}
+
+function addLatLng(event){  
+    var path = polyg.getPath();
+    // Because path is an MVCArray, we can simply append a new coordinate
+    // and it will automatically appear.
+    path.push(event.latLng);
+    
+    console.log(path);
+}
+var linePath;
+function createPolyline(map,lineCoordinates,lineSymbol){
+    linePath = new google.maps.Polyline({
+    path: lineCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+    editable: true,
+     icons: [{ // this Array is for adding symbols to the line
+      icon: lineSymbol,
+      offset: '0',
+      repeat: '20px'
+    }]
   });
-  return infowindow;
+   linePath.setMap(map);
 }
-
-function getCurrentPosition(e){
-  var markercoords = new google.maps.LatLng(e.latLng.lat(),e.latLng.lng());
-  return markercoords;
-}
-
-var marker;
-function createMarker(coords, map, title){
-    marker = new google.maps.Marker({
-    position: coords,
-    map: map,
-    title: title,
-    draggable: true,
-    animation: google.maps.Animation.DROP
-  });
-    return marker;
-}
-function createCustomMarker(coords,map,title){
-    marker = new google.maps.Marker({
-    position: coords,
-    map: map,
-    title: title,
-    icon: createImage("/assets/icon.png"),
-    draggable: true,
-    animation: google.maps.Animation.DROP
-  }); 
-    return marker;
-}
-//creating image for custom marker
-function createImage(url){
-    var image = {
-    url: url,
-    // This marker is 20 pixels wide by 32 pixels tall.
-    size: new google.maps.Size(32, 32),
-    // The origin for this image is 0,0.
-    origin: new google.maps.Point(0,0),
-    // The anchor for this image is the base of the flagpole at 0,32.
-    anchor: new google.maps.Point(0, 32)
-  };
-  return image;
-}
-
 
 function loadScript() {
 	console.log("map loading ...");
