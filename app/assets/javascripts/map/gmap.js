d285b228a6265c358b3e1c5e2981d7e7c3a5a750
Nouran Mahmoud <nouran.mhmoud@gmail.com>
2014-07-04 15:45:06 +0000

    Creating custom map style

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -5,37 +5,50 @@ $(window).load(function() {
 var map;
 var poly;
 function initialize() {
-        
-        var directionsService = new google.maps.DirectionsService(); 
-        var directionsDisplay = new google.maps.DirectionsRenderer(); 
-  
+
+  var mapstyle = [
+                    {
+                      "featureType": "administrative.locality",
+                      "elementType": "labels.icon",
+                      "stylers": [
+                        { "invert_lightness": true },
+                        { "color": "#e40952" },
+                        { "visibility": "on" }
+                      ]
+                    },{
+                      "featureType": "water",
+                      "elementType": "geometry.fill",
+                      "stylers": [
+                        { "visibility": "on" },
+                        { "hue": "#5eff00" },
+                        { "color": "#282744" },
+                        { "weight": 0.1 },
+                        { "saturation": -56 },
+                        { "lightness": 22 },
+                        { "gamma": 3.91 }
+                      ]
+                    }
+                  ]
+  var styledMap = new google.maps.StyledMapType(mapstyle,{name: "styled map"});
   var mapOptions = {
           center: new google.maps.LatLng(30.055487, 31.279766),
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.NORMAL,
           panControl: true,
+            mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, "map_style"]
+          },
           scaleControl: false,
           streetViewControl: true,
           overviewMapControl: true
         };
         // initializing map
         map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-        directionsDisplay.setMap(map);
-        var request =
-                {
-                        origin: "Mansoura, Daqahlia, Egypt",
-                        destination: "Cairo, Egypt",
-                        travelMode: google.maps.DirectionsTravelMode.DRIVING
-                };
-        directionsService.route(request, function(response, status)
-            {
-                 if (status == google.maps.DirectionsStatus.OK) //Check if request is successful.
-                    {
-                      console.log(status);
-                      directionsDisplay.setDirections(response); //Display the directions result
-                    }
-            });
-}
+        map.mapTypes.set("map_style",styledMap);
+        map.setMapTypeId("map_style");
+ }
+
 function loadScript() {
 	console.log("map loading ...");
   var script = document.createElement('script');
