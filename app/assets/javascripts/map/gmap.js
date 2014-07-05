5d7ff7d88073d2baa207bbc1ff735846cf888153
Nouran Mahmoud <nouran.mhmoud@gmail.com>
2014-07-05 13:16:40 +0000

    Geocoding and reverse geocoding

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -5,67 +5,100 @@ $(window).load(function() {
 var map;
 
 function initialize() {
-       
+        
   var mapOptions = {
           center: new google.maps.LatLng(30.055487, 31.279766),
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.NORMAL,
           panControl: true,
-          zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-            position: google.maps.ControlPosition.LEFT_CENTER
-          },
-          mapTypeControlOptions: {
-            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, "map_style"]
-          },
           scaleControl: false,
           streetViewControl: true,
           overviewMapControl: true
         };
-  // initializing map
-  map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-  //default control constructed by inner css
-  var homeControlDiv = document.createElement('div');
-  var homeControl = new HomeControl(homeControlDiv, map);
-  homeControlDiv.index = 1;
-  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
-  // control constructed by outer css
-  var controlDiv =$("#control-div");
-  var controlUI = $("#control-ui");
-  var controlText = $("#control-text");
-  controlUI.click(function() {
-    map.setZoom(11);
-  });
-  controlDiv.index = 1;
-  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(controlDiv[0]);
-
+        // initializing map
+        map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+        
+   // geocoding 
+      var geocoding  = new google.maps.Geocoder();
+      $("#submit_button_geocoding").click(function(){
+        codeAddress(geocoding);
+      });
+      $("#submit_button_reverse").click(function(){
+        codeLatLng(geocoding);
+      });
+      
+   
 }
 
-function HomeControl(controlDiv, map) {
- controlDiv.style.padding = '5px';
-  
-  var controlUI = document.createElement('div');
-  controlUI.style.backgroundColor = 'white';
-  controlUI.style.borderStyle = 'solid';
-  controlUI.style.borderWidth = '2px';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to set the map to zoom level 11';
-  controlDiv.appendChild(controlUI);
+var info;
+function codeLatLng(geocoding){
 
-  var controlText = document.createElement('div');
-  controlText.style.fontFamily = 'Arial,sans-serif';
-  controlText.style.fontSize = '12px';
-  controlText.style.paddingLeft = '4px';
-  controlText.style.paddingRight = '4px';
-  controlText.innerHTML = '<b>Fullscreen</b>';
-  controlUI.appendChild(controlText);
-  google.maps.event.addDomListener(controlUI, 'click', function() {
-    $("#map-canvas").toggleClass("fullscreen");
-    google.maps.event.trigger(map , 'resize');
-    controlText.innerHTML = $("#map-canvas").hasClass("fullscreen")? '<b>Normal Screen</b>':'<b>FullScreen</b>';
+  var input = $('#search_box_reverse').val();
+  console.log(input);
+  
+  var latlngbounds = new google.maps.LatLngBounds();
+  var listener;
+  var regex = /([1-9])+\.([1-9])+\,([1-9])+\.([1-9])+/g;
+  if(regex.test(input)) {
+  var latLngStr = input.split(",",2);
+  var lat = parseFloat(latLngStr[0]);
+  var lng = parseFloat(latLngStr[1]);
+  var latLng = new google.maps.LatLng(lat, lng);
+  geocoding.geocode({'latLng': latLng}, function(results, status) {
+     if (status == google.maps.GeocoderStatus.OK){
+       if(results.length > 0){
+         //map.setZoom(11);
+         var marker;
+         map.setCenter(results[1].geometry.location);
+         var i;
+        info = createInfoWindow("");
+         for(i in results){
+           latlngbounds.extend(results[i].geometry.location);
+             marker = new google.maps.Marker({
+             map: map,
+             position: results[i].geometry.location
+           });
+          
+          google.maps.event.addListener(marker, 'click', (function(marker,i) {
+            return function() {
+            info.setContent(results[i].formatted_address);
+            info.open(map,marker);
+            }
+          })(marker,i));
+        }
+
+         map.fitBounds(latlngbounds);
+         listener = google.maps.event.addListener(map, "idle", function() {
+          if (map.getZoom() > 16) map.setZoom(16);
+            google.maps.event.removeListener(listener);
+          });
+       }
+     }
+    else{
+       alert("Geocoder failed due to: " + status);
+     }  
   });
+  }else{
+    alert("Wrong lat,lng format!");
+  }
+}
+function codeAddress(geocoding){
+  var address = $("#search_box_geocoding").val();
+  if(address.length > 0){
+    geocoding.geocode({'address': address},function(results, status){
+      if(status == google.maps.GeocoderStatus.OK){
+        map.setCenter(results[0].geometry.location);
+        var marker  =  new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+        }else{
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }else{
+    alert("Search field can't be blank");
+  }
 }
 
 function loadScript() {
